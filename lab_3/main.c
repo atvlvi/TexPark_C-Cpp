17741d3a669f519f5398856f34539531571f67ec
viewsharp <viewsharp@yandex.ru>
2017-10-10 17:52:23 +0300

    ytrewq

--- lab_3/main.c
+++ lab_3/main.c
@@ -10,6 +10,10 @@ int sort_list(node *list);
 int insert(node *list, char *in);
 void print_list(node *list);
 
+void destruct(void *value) {
+    free_word(value);
+}
+
 int main(int argc, char *argv[]) {
     switch (argc) {
         case 1:
@@ -30,7 +34,7 @@ int main(int argc, char *argv[]) {
     char string[MAXLEN];
     char *link = string;
     char *max_link = string + MAXLEN - 1;
-    node *list = new_node(NULL, free_word);
+    node *list = new_node(NULL, destruct);
     int success = 0;
 
     while (fread(buf, sizeof(char), MAXLEN, fin) == MAXLEN) {
@@ -63,7 +67,7 @@ int insert(node *list, char *in) {
         list->value = new_word(in);
         if (!list->value)
             return 0;
-        list->next = new_node(NULL, free_word);
+        list->next = new_node(NULL, destruct);
         if (!list->next)
             return 0;
         return 1;
