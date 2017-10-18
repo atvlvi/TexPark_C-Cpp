e65a89517b78d81a2fb69b88b0c278a8615d39a6
viewsharp <viewsharp@yandex.ru>
2017-10-18 20:57:22 +0300

    to_string

--- lab_4/main.cpp
+++ lab_4/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
 #include "account.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,11 +13,12 @@ int main(int argc, char** argv) {
     Account *array = new Account[ACCOUNT_COUNT];
     char buf[16];
     char num[8];
+    std::string s;
 
     for (int i = 0; i < ACCOUNT_COUNT; i++) {
         strncpy(buf, "Name", 16);
-        itoa(rand()%12, num, 10);
-        strncat(buf, num, 16);
+        s = std::to_string(rand()%12);
+        strncat(buf, s.c_str(), 16);
         if (i % 8 == 0)
             array[i] = *(new Account(i, buf, rand(), rand()));
         else
